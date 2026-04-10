c1d90cafd5eb21c22685ab3bbc814124445545ce
Augusto <guilhermesantos172007@gmail.com>
2026-04-10 09:11:57 -0300

    Adição do cabeçalho à primeira função

--- listah.c
+++ listah.c
@@ -32,10 +32,24 @@ int AdicionarTribunal(Lista *L, Tribunal A) {
     return 1;
 }
 
-void EscreverCabecalho(int comando){
-    if(comando = 1){
-        
+int EscreverCabecalho(int comando, const char *NomeArquivo){
+    if(comando == 0){
+        FILE * D;
+        D = fopen(NomeArquivo, "w");
+        if (D == NULL)  {
+            printf("ERRO: O arquivo não foi devidamente aberto");
+        }
+        fprintf(D, "\"sigla_tribunal\",\"procedimento\",\"ramo_justica\",\"sigla_grau\","
+           "\"uf_oj\",\"municipio_oj\",\"id_ultimo_oj\",\"nome\",\"mesano_cnm1\",\"mesano_sent\","
+           "\"casos_novos_2026\",\"julgados_2026\",\"prim_sent2026\",\"suspensos_2026\","
+           "\"dessobrestados_2026\",\"cumprimento_meta1\",\"distm2_a\",\"julgm2_a\",\"suspm2_a\","
+           "\"cumprimento_meta2a\",\"distm2_ant\",\"julgm2_ant\",\"suspm2_ant\",\"desom2_ant\","
+           "\"cumprimento_meta2ant\",\"distm4_a\",\"julgm4_a\",\"suspm4_a\",\"cumprimento_meta4a\","
+           "\"distm4_b\",\"julgm4_b\",\"suspm4_b\",\"cumprimento_meta4b\"\n");
+        fclose(D);
     }
+    
+    return 1;
 }
 
 int CarregarCSV(Lista *L, const char *Nome_Arquivo, Tribunal t) {
@@ -52,6 +66,7 @@ int CarregarCSV(Lista *L, const char *Nome_Arquivo, Tribunal t) {
     int lidos = 0;
     while (fgets(linha, sizeof(linha), F) != NULL) {
         if (strlen(linha) <= 1) continue;
+        linha[strcspn(linha, "\r\n")] = '\0';
 
         sscanf(linha,
             "\"%[^\"]\","   // t.sigla_tribunal
@@ -137,15 +152,19 @@ int EscreverCSV(Lista *L, Tribunal t, const char *Nome_Arquivo)  {
 }
 
 int ConcatenarDados(Lista *L, Tribunal T, const char *NomeArquivo){
-    int n_arquivos = 27;
-    char *arquivos[] = {"teste_TRE-AC.csv", "teste_TRE-AL.csv", "teste_TRE-AM.csv", "teste_TRE-AP.csv", "teste_TRE-BA.csv", "teste_TRE-CE.csv", "teste_TRE-DF.csv", "teste_TRE-ES.csv", "teste_TRE-GO.csv",
-        "teste_TRE-MA.csv", "teste_TRE-MG.csv", "teste_TRE-MS.csv", "teste_TRE-MT.csv", "teste_TRE-PA.csv", "teste_TRE-PB.csv", "teste_TRE-PE.csv", "teste_TRE-PI.csv", "teste_TRE-PR.csv",
-        "teste_TRE-RJ.csv", "teste_TRE-RN.csv", "teste_TRE-RO.csv", "teste_TRE-RR.csv", "teste_TRE-RS.csv", "teste_TRE-SC.csv", "teste_TRE-SE.csv", "teste_TRE-SP.csv", "teste_TRE-TO.csv"
-    }; 
+    int n_arquivos = 1, header = 0; // São 27 
+    char *arquivos[] = {"teste_TRE-AC.csv", "teste_TRE-AL.csv"};
+    // , "teste_TRE-AM.csv", "teste_TRE-AP.csv", "teste_TRE-BA.csv", "teste_TRE-CE.csv", "teste_TRE-DF.csv", "teste_TRE-ES.csv", "teste_TRE-GO.csv",
+    //     "teste_TRE-MA.csv", "teste_TRE-MG.csv", "teste_TRE-MS.csv", "teste_TRE-MT.csv", "teste_TRE-PA.csv", "teste_TRE-PB.csv", "teste_TRE-PE.csv", "teste_TRE-PI.csv", "teste_TRE-PR.csv",
+    //     "teste_TRE-RJ.csv", "teste_TRE-RN.csv", "teste_TRE-RO.csv", "teste_TRE-RR.csv", "teste_TRE-RS.csv", "teste_TRE-SC.csv", "teste_TRE-SE.csv", "teste_TRE-SP.csv", "teste_TRE-TO.csv"
+    // }; 
     for(int i = 0; i < n_arquivos; i++) {
+        EscreverCabecalho(header, NomeArquivo);
         int carregados =    CarregarCSV(L, arquivos[i], T);
         int escritos =      EscreverCSV(L, T, NomeArquivo);
         L->Tamanho = 0;
+        header = 1;
+
 
         if (escritos != carregados){
             printf("Houve um deficit de %d na escrita do arquivo %s", carregados - escritos, arquivos[i]);
